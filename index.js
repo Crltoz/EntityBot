bb479b4ed87eb4393f04e1068b89c50370ca0dd0
Crltoz <mcarlos458@gmail.com>
2021-08-24 14:11:35 -0300

    fix commands count

--- index.js
+++ index.js
@@ -504,9 +504,9 @@ client.on("message", async (message) => {
   if (message.content.startsWith(prefix[message.guild.id])) {
 
     if (lenguaje[message.guild.id] == 0) {
-      con.query(`UPDATE language SET count = count + 1 WHERE name = 'spanish'`);
+      con.query(`UPDATE commands SET count = count + 1 WHERE name = 'spanish'`);
     } else {
-      con.query(`UPDATE language SET count = count + 1 WHERE name = 'english'`);
+      con.query(`UPDATE commands SET count = count + 1 WHERE name = 'english'`);
     }
 
     console.log(`${message.author.tag} command: ${message.content}`)
