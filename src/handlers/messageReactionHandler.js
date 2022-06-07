76ac2dcde0833d4890a6089f8adb2995113b2085
Crltoz <43712486+Crltoz@users.noreply.github.com>
2022-06-07 14:58:19 -0300

    fix client id

--- src/handlers/messageReactionHandler.js
+++ src/handlers/messageReactionHandler.js
@@ -2,7 +2,7 @@ const texts = require("../data/texts.json");
 
 function messageReactionHandler(context, reaction, user) {
     const serverConfig = context.client.servers.get(reaction.message.guildId);
-    if (serverConfig && serverConfig.prefix != "/" && reaction.emoji.name == "✅" && reaction.message.author.id === context.client.id) {
+    if (serverConfig && serverConfig.prefix != "/" && reaction.emoji.name == "✅" && reaction.message.author.id === context.client.user.id) {
         reaction.message.channel.send(texts.dailyMessageDisabled[serverConfig.language]);
         serverConfig.prefix = "/";
         context.services.database.query(`UPDATE Servidores SET prefix = '/' WHERE ID = ${reaction.message.guildId}`);
