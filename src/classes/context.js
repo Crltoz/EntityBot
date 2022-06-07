7f5250eee96222710bfe308772ee698da6ac5a16
Crltoz <43712486+Crltoz@users.noreply.github.com>
2022-06-07 14:52:47 -0300

    add message reaction intents

--- src/classes/context.js
+++ src/classes/context.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 });
 
