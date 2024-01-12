56c61d20531fc0f8c9113db5c83080e12e6ba2e3
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-01-12 12:09:55 -0300

    migrate hardcoded strings

--- src/commands/sv.js
+++ src/commands/sv.js
@@ -1,21 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const texts = require('../data/texts.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('sv')
     .setDescription('Get count servers that the bot is in'),
   async execute(context, interaction) {
-    if (interaction.member.id != '277506787261939712' && interaction.member.id != '313496742156959745' && interaction.member.id != '389320439932911626' && interaction.member.id != '169818091281186816') return interaction.member.send('SACA LA MANO DE AHÍ CARAJO', {
-      files: [{
-        attachment: 'https://i.ytimg.com/vi/7A6FricobFA/hqdefault.jpg',
-        name: "SACA_LA_MANO.jpg"
-      }]
-    });
+    const serverConfig = await context.services.database.getOrCreateServer(interaction.guildId)
     let users = 0;
     context.client.guilds.cache.forEach((guild) => {
       users += guild.memberCount;
     });
-    await interaction.reply('Estoy actualmente en **' + context.client.guilds.cache.size + '** servidores.');
-    await interaction.followUp('Y **' + users + '** usuarios tienen acceso a mis funcionalidades.');
+    await interaction.reply(texts.servers[serverConfig.language] + '**' + context.client.guilds.cache.size + '**');
+    await interaction.followUp('**' + users + '** ' + texts.users[serverConfig.language]);
   },
 };
