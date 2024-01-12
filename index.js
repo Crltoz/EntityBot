cde46ec87c89f392b0c81bb1d2460e3a281dad80
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-01-12 01:52:51 -0300

    remove unused logic

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
+require('dotenv').config();
+
 const ContextConstructor = require("./src/classes/context.js");
 const context = new ContextConstructor();
 
+
 // handlers
 const interactionHandler = require("./src/handlers/interactionHandler.js");
-const messageHandler = require("./src/handlers/messageHandler.js");
 const guildHandler = require("./src/handlers/guildHandler.js");
-const messageReactionHandler = require("./src/handlers/messageReactionHandler.js");
 
 context.client.on("ready", () => { 
     context.services.interactions.init(context);
-    context.services.database.init(context);
     context.services.perks.init();
     context.services.stats.init();
     context.services.characters.init();
     context.services.rules.init();
+    context.services.database.init();
     console.log(`Client ${context.client.user.username} loading!`);
 });
 
 context.client.on("interactionCreate", (interaction) => { interactionHandler(context, interaction); });
 
-//context.client.on("messageCreate", (message) => { messageHandler(context, message); });
-
 context.client.on("guildCreate", (guild) => { guildHandler(context, guild); });
 
-//context.client.on("messageReactionAdd", (reaction, user) => { messageReactionHandler(context, reaction, user) });
-
-context.client.login(context.config.token);
+context.client.login(process.env.TOKEN);
 
 module.exports = {
     context: context
